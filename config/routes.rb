deeecd660095ff0f9b64ce7c8f8ec6124631565e
tatsuya oozeki <tasuya6281@icloud.com>
2019-10-23 04:46:56 +0900

    Add: devise:user,user,staff table controller model create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  get 'home/index'
+  get 'home/mypage'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
 end
