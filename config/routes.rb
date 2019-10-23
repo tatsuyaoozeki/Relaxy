1adf8f83c1fa7dc046e4f1ca6b5a74d76b7ecbb5
tatsuya oozeki <tasuya6281@icloud.com>
2019-10-23 13:47:07 +0900

    Add: routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
-  get 'staffs/index'
-  get 'users/show'
-  get 'home/index'
-  get 'home/mypage'
   devise_for :users, controllers: {
+        registrations: 'users/registrations',
         sessions: 'users/sessions'
       }
+  devise_scope :users do
+    get "user/:id", to: "users/registrations#detail"
+    get "signup", to: "users/registrations#new"
+    get "signout", to: "users/registrations#new"
+    get "login", to: "users/sessions#new"
+    get "logout", to: "users/sessions#destroy"
+  end
+
+  resources :users, only: [:new, :create, :show] do
+    resources :staffs
+  end
+
+  get 'users/show'
+  root 'home#index'
+  post   'users/:user_id/staffs/:id', to: 'staffs#show'
 end
