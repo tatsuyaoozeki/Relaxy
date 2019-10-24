b9215ccab364b2cc9b5407ee5188d424f58ddae3
tatsuya oozeki <tasuya6281@icloud.com>
2019-10-24 15:42:46 +0900

    Add: reservation controller, menus controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'menus/index'
   devise_for :users, controllers: {
         registrations: 'users/registrations',
         sessions: 'users/sessions'
