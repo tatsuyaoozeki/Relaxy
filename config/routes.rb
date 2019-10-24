3a4a3bb4fbd82da9e2664ce3eb72c631ce2a694c
tatsuyaoozeki <51943989+tatsuyaoozeki@users.noreply.github.com>
2019-10-24 15:11:29 +0900

    Revert "Topic#2　ユーザーが気になったスタッフをフォローする機能作成"

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        registrations: 'users/registrations',
-        sessions: 'users/sessions'
-      }
-  devise_scope :users do
-    get "user/:id", to: "users/registrations#detail"
-    get "signup", to: "users/registrations#new"
-    get "signout", to: "users/registrations#new"
-    get "login", to: "users/sessions#new"
-    get "logout", to: "users/sessions#destroy"
-  end
-
-  resources :users, only: [:show] do
-    resources :staffs
-  end
-
-  resources :relationships, only: [:create, :destroy]
-
-  get 'users/show'
-  root 'home#index'
-  post 'users/:user_id/staffs/:id', to: 'staffs#show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/staffs_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class StaffsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get staffs_index_url
-    assert_response :success
-  end
-
-end
