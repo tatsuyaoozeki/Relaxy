dd1191c4b55488dd50a0c99bde9a2384a15c33f1
tatsuya oozeki <tasuya6281@icloud.com>
2019-10-24 16:59:50 +0900

    Add: gem <---- bootstrap4,jquery,datetimepicker,moment.js,font-awesome

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,62 @@
 class MenusController < ApplicationController
+  before_action :set_menu, only: [:show, :edit, :update, :destroy]
+
+  # GET /menus
+  # GET /menus.json
   def index
+    @menus = Menu.all
   end
+
+  # GET /menus/1
+  # GET /menus/1.json
+  def show
+  end
+
+  # GET /menus/new
+  def new
+    @menu = Menu.new
+  end
+
+  # GET /menus/1/edit
+  def edit
+  end
+
+  # POST /menus
+  # POST /menus.json
+  def create
+    @menu = Menu.new(menu_params)
+
+
+      if @menu.save
+        
+      else
+
+      end
+    end
+  end
+
+  # PATCH/PUT /menus/1
+  # PATCH/PUT /menus/1.json
+  def update
+
+  end
+
+  # DELETE /menus/1
+  # DELETE /menus/1.json
+  def destroy
+    @menu.destroy
+
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_menu
+      @menu = Menu.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def menu_params
+      params.require(:menu).permit(:name, :time_require, :price, :content)
+    end
+end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,16 @@
 class ReservationsController < ApplicationController
   def index
+    @reservations = Reservation.all.reverse
+  end
+
+  def create
+    @reservation = current_user.reservations.create(reservation_params)
+    redirect_to root_path notice:"予約が完了しました"
+    end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :menu_id)
   end
 end
