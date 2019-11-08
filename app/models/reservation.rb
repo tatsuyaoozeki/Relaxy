a34a59f6eff8daaea2c324c5c1b70503bf20e19f
tatsuya oozeki <tasuya6281@icloud.com>
2019-11-08 13:48:11 +0900

    Fix: view/menuを修正

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,17 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :menu, optional: true
   validates :start_date, presence: true
-  validates :end_date, presence: true
   validate :date_before_start
-  validate :date_before_end
+
 
   def date_before_start
-    errors.add(:start_date, "の日付を正しく記入して下さい。") if
+    errors.add(:start_date, "は今日以降のものを選択してください") if
     start_date < Date.today
-  end
-
-  def date_before_end
-    errors.add(:end_date, "の日付を正しく記入して下さい。") if
-    end_date < Date.today
   end
 end
