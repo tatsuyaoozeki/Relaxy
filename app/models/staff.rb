9c310ba25c2ad11c8406226fd59a93b38464da30
tatsuya oozeki <tasuya6281@icloud.com>
2019-11-22 18:38:04 +0900

    Fix: 性別がtrueと表示させないようにgenderのデータ型をintegerに変更、enumを使って男性・女性を表示に変更

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -3,5 +3,6 @@ class Staff < ApplicationRecord
   validates :user_id, presence: true, uniqueness: true
   validates :content, presence: true, length: { maximum: 600 }
   validates :gender, presence: true
+  enum gender: {男性: 0, 女性: 1}
   has_many :following, dependent: :destroy
 end
