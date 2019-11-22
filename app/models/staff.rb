229b3c9d900c3f1d17e0f8b1d292f789d8b83c73
tatsuya oozeki <tasuya6281@icloud.com>
2019-11-22 19:07:29 +0900

    Fix: genderのenumを変更

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -2,7 +2,8 @@ class Staff < ApplicationRecord
   belongs_to :user, optional: true
   validates :user_id, presence: true, uniqueness: true
   validates :content, presence: true, length: { maximum: 600 }
-  validates :gender, presence: true
+  # validates :gender, presence: true
   enum gender: {男性: 0, 女性: 1}
+  validates :status, inclusion: {in: ["男性", "女性"]}
   has_many :following, dependent: :destroy
 end
