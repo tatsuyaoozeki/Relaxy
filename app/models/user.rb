b61ed96eea2e12a67694efba1666a8c5da4bbb44
tatsuya oozeki <tasuya6281@icloud.com>
2019-11-22 16:37:49 +0900

    Fix: deviseで登録したユーザーの編集機能を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,17 @@ class User < ApplicationRecord
   def unfollow!(other_user)
     following_relationships.find_by(following_id: other_user.id).destroy
   end
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
 end
