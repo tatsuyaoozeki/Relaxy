dd61bea42f3d2ee6627697279f95f278f153cc92
tatsuya oozeki <tasuya6281@icloud.com>
2019-11-25 18:00:39 +0900

    fix: views/home get startをログイン時は非表示にする

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   devise_for :users, controllers: {
         registrations: 'users/registrations',
         sessions: 'users/sessions'
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
     get "logout", to: "users/sessions#destroy"
   end
 
-  root 'home#index'
+
   # post 'users/:user_id/staffs/:id', to: 'staffs#show'
 
   resources :feeds
